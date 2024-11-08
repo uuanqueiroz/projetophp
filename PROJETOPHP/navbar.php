d7830cc099b01c5bebe86a567bfc02558950168b
uuanqueiroz <47960412+uuanqueiroz@users.noreply.github.com>
2024-11-08 16:58:00 -0300

    Delete PROJETOPHP directory
    Delete PROJETOPHP directory

--- PROJETOPHP/navbar.php
+++
@@ -1,5 +0,0 @@
-<nav class="navbar navbar-dark bg-dark fixed-top">
-    <div class="container-fluid d-flex justify-content-center"> 
-        <a class="navbar-brand" href="#">PROJETO DO PROFESSOR LEANDRO</a>
-    </div>
-</nav>
