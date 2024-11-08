fb387b01972d0d7b36014eef02af35cff97a5f25
uuanqueiroz <47960412+uuanqueiroz@users.noreply.github.com>
2024-11-08 14:32:40 -0300

    adicionando cod
    projeto completo com as views, controladores em acoes.php.

---
+++ PROJETOPHP/navbar.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-dark bg-dark fixed-top">
+    <div class="container-fluid d-flex justify-content-center"> 
+        <a class="navbar-brand" href="#">PROJETO DO PROFESSOR LEANDRO</a>
+    </div>
+</nav>
---
+++ PROJETOPHP/usuario-view.php
@@ -0,0 +1,74 @@
+<?php
+require 'conexao.php';
+?>
+    <!doctype html>
+    <html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="interactive spider.css">
+    <title>Usuário - Visualizar</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body class="d-flex flex-column min-vh-100">
+<?php include('navbar.php'); ?>
+<main class = "flex-grow-1">
+<div class="container mt-5 conteudo-tamanho">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Visualizar usuário
+                        <a href="index.php" class="btn btn-danger float-end">Voltar</a>
+                    </h4>
+                </div>
+                <div class="card-body">
+                    <?php
+                    if (isset($_GET['id'])) {
+                        $usuario_id = mysqli_real_escape_string($conexao, $_GET['id']);
+                        $sql = "SELECT * FROM usuarios WHERE id='$usuario_id'";
+                        $query = mysqli_query($conexao, $sql);
+                        if (mysqli_num_rows($query) > 0) {
+                            $usuario = mysqli_fetch_array($query);
+                            ?>
+                            <div class="mb-3">
+                                <label>Nome</label>
+                                <p class="form-control">
+                                    <?=$usuario['nome'];?>
+                                </p>
+                            </div>
+                            <div class="mb-3">
+                                <label>Email</label>
+                                <p class="form-control">
+                                    <?=$usuario['email'];?>
+                                </p>
+                            </div>
+                            <div class="mb-3">
+                                <label>Data Nascimento</label>
+                                <p class="form-control">
+                                    <?=date('d/m/Y', strtotime($usuario['data_nascimento']));?>
+                                </p>
+                            </div>
+                            <?php
+                        } else {
+                            echo "<h5>Usuário não encontrado</h5>";
+                        }
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</main> 
+<footer class="bg-dark text-light text-center py-3">
+  <div class="container">
+    <p>&copy; <?= date("Y"); ?> CÓPIA NAO COMEDIA, DEU TRABALHO FAZER</p>
+  </div>
+</footer>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<canvas id="canvas"> </canvas>
+<script src="interactive spider.js"></script>
+</body>
+    </html><?php
